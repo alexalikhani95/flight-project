94090fd70e86259e731f8307f219db0bbe828b2f
Alex <alikhanialex@gmail.com>
2023-04-14 21:13:59 +0100

    Dont show the login form is user is already signed in

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,31 @@
 'use client';
 import AuthForm from './components/AuthForm';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import { UserContext, UserContextType } from './context/UserContext';
 
 export default function Home() {
   const [signIn, setSignIn] = useState(true);
+  const { user } = useContext(UserContext) as UserContextType;
 
   return (
     <div className="flex w-full bg-gray-100 h-screen items-center flex-col">
-      <div className="flex items-center justify-center mt-10">
-        <p>{signIn ? 'Dont have an account?' : 'Already have an account?'}</p>
-        <button
-          className="bg-red-500 hover:bg-red-700 text-white font-bold rounded p-2 ml-2"
-          onClick={() => setSignIn(!signIn)}
-        >
-          {signIn ? 'Signup' : 'Login'}
-        </button>
-      </div>
-      <AuthForm isLogin={signIn} />
+      <h1 className="text-3xl font-bold">Home</h1>
+      {!user && (
+        <>
+          <div className="flex items-center justify-center mt-10">
+            <p>
+              {signIn ? 'Dont have an account?' : 'Already have an account?'}
+            </p>
+            <button
+              className="bg-red-500 hover:bg-red-700 text-white font-bold rounded p-2 ml-2"
+              onClick={() => setSignIn(!signIn)}
+            >
+              {signIn ? 'Signup' : 'Login'}
+            </button>
+          </div>
+          <AuthForm isLogin={signIn} />
+        </>
+      )}
     </div>
   );
 }
