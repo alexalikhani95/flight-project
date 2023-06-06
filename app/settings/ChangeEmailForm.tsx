d0aa3591caa6483180960ea8b399248fad6663d6
Alex <alikhanialex@gmail.com>
2023-06-06 21:30:20 +0100

    Handling errors for change password and change email

--- app/settings/ChangeEmailForm.tsx
+++ app/settings/ChangeEmailForm.tsx
@@ -10,12 +10,38 @@ type ChangeEmailData = {
 
 const ChangeEmailForm = () => {
   const { changeEmail, user } = useContext(UserContext) as UserContextType;
-  const { register, handleSubmit } = useForm<ChangeEmailData>();
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<ChangeEmailData>();
   const [showEmailUpdatedText, setShowEmailUpdatedText] = useState(false);
 
-  const onSubmit = (data: ChangeEmailData) => {
-    changeEmail(data.newEmail);
-    setShowEmailUpdatedText(true);
+  const onSubmit = async (data: ChangeEmailData) => {
+    try {
+      await changeEmail(data.newEmail);
+      setShowEmailUpdatedText(true);
+    } catch (error: any) {
+      console.log(error.code);
+      if (error.code === 'auth/email-already-in-use') {
+        return setError('newEmail', {
+          type: 'manual',
+          message: 'A user with this email already exists',
+        });
+      }
+      if (error.code === 'auth/invalid-email') {
+        return setError('newEmail', {
+          type: 'manual',
+          message: 'Invalid email',
+        });
+      } else {
+        setError('newEmail', {
+          type: 'manual',
+          message: 'An error occurred. Please try again later.',
+        });
+      }
+    }
   };
 
   useEffect(() => {
@@ -30,13 +56,18 @@ const ChangeEmailForm = () => {
         <p className="font-bold">Change Email</p>
         <form onSubmit={handleSubmit(onSubmit)} className="p-5 bg-white">
           <div className="mb-5 flex flex-col">
-            <label htmlFor="email">New Email</label>
-            <input
-              type="text"
-              id="change-email"
-              {...register('newEmail', { required: true })}
-              className="shadow border rounded py-2 px-3"
-            />
+            <label htmlFor="email">
+              New Email
+              <input
+                type="text"
+                id="change-email"
+                {...register('newEmail', { required: true })}
+                className="shadow border rounded py-2 px-3 ml-2"
+              />
+            </label>
+            {errors.newEmail && (
+              <p className="text-red-500 mt-1">{errors.newEmail.message}</p>
+            )}
             {showEmailUpdatedText && <p>Email Updated!</p>}
           </div>
 
