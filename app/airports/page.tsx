a6b9457f65a6f7d98865ce8d96e33ffe9e109969
Alex <alikhanialex@gmail.com>
2023-06-12 20:52:38 +0100

    Using debounce to delay filtered airports from showing before the user has finished typing in the search input

--- app/airports/page.tsx
+++ app/airports/page.tsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { AirportData } from '@/types/types';
 import { useQuery } from '@tanstack/react-query';
+import { debounce } from 'lodash';
 
 const fetchAirports = async () => {
   const { data } = await axios.get<AirportData[]>('/api/airports');
@@ -21,19 +22,24 @@ const Airports: React.FC = () => {
 
   const [filteredAirports, setFilteredAirports] = useState<AirportData[]>([]);
 
+  const [finishedSearch, setFinishedSearch] = useState(false);
+
+  const delayedSearch = debounce((search: string) => {
+    if (airports) {
+      const filtered = airports.filter((airport) =>
+        airport.name?.toLowerCase().includes(search.toLowerCase())
+      );
+      setFilteredAirports(filtered);
+      setFinishedSearch(true);
+    }
+  }, 1000); // Delayed debounce for 1 second
+
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const search = event.target.value;
     setSearchInput(search);
+    delayedSearch.cancel(); // Cancel any existing debounce timers when the search input changes.
+    delayedSearch(search); // Call the delayed search function
   };
-
-  React.useEffect(() => {
-    if (airports) {
-      const filtered = airports.filter((airport) =>
-        airport.name?.toLowerCase().includes(searchInput.toLowerCase())
-      );
-      setFilteredAirports(filtered);
-    }
-  }, [airports, searchInput]);
 
   return (
     <div className="flex flex-col text-blue-950 items-center">
@@ -46,17 +52,26 @@ const Airports: React.FC = () => {
         className="mt-4 p-2 border border-gray-300 rounded"
       />
       {isLoading && <p>Loading airports...</p>}
-      {!isLoading && airports?.length === 0 && (
-        <p>No airports found, please try again.</p>
-      )}
+      {airports?.length === 0 && <p>No airports found, please try again.</p>}
       {isError && <p>Error, please try again later</p>}
-      {filteredAirports.length < 1 && (
+      {filteredAirports.length < 1 && searchInput !== '' && finishedSearch && (
         <p className="mt-2 text-red-500">
           No airports found matching the search criteria.
         </p>
       )}
-      {!isLoading &&
+      {filteredAirports.length > 0 &&
         filteredAirports?.map((airport, index) => (
+          <div
+            key={index}
+            className="flex flex-col align-center mt-10 shadow-lg w-[300px] max-w-full bg-white p-5"
+          >
+            <p className="font-bold">{airport.name}</p>
+          </div>
+        ))}
+      {filteredAirports.length < 1 &&
+        searchInput === '' &&
+        airports &&
+        airports.map((airport, index) => (
           <div
             key={index}
             className="flex flex-col align-center mt-10 shadow-lg w-[300px] max-w-full bg-white p-5"
