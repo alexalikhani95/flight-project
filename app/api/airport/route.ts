adddd360f6bb97833b21e86f8ccbb7aab52a38f0
Alex <alikhanialex@gmail.com>
2023-07-19 19:56:55 +0100

    Adding buttons in a flight card to link to airport info of the arrival/departure airport

--- app/api/airport/route.ts
+++ app/api/airport/route.ts
@@ -7,8 +7,6 @@ export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
     const iata_code = searchParams.get('iata_code');
 
-    // return NextResponse.json({ iata_code });
-
     if (!iata_code) {
       // Handle the case when `iata_code` is missing or not provided
       return new Response('iata_code is required', { status: 400 });
@@ -16,8 +14,6 @@ export async function GET(request: Request) {
 
     console.log(iata_code);
     console.log(process.env.AIRLABS_API_KEY);
-
-    // https://airlabs.co/api/v9/airports?iata_code=CDG&api_key=c0756ec2-5735-4a75-90e8-fa17e281ad48
 
     const { data } = await axios.get(
       `https://airlabs.co/api/v9/airports?iata_code=${iata_code}&api_key=${process.env.AIRLABS_API_KEY}`
@@ -29,19 +25,3 @@ export async function GET(request: Request) {
     return new Response('failed to load', { status: 500 });
   }
 }
-
-// import { NextResponse } from 'next/server'
-
-// export async function GET(request: Request) {
-// const { searchParams } = new URL(request.url)
-// const id = searchParams.get('id')
-//   const res = await fetch(`https://data.mongodb-api.com/product/${id}`, {
-//     headers: {
-//       'Content-Type': 'application/json',
-//       'API-Key': process.env.DATA_API_KEY,
-//     },
-//   })
-//   const product = await res.json()
-
-//   return NextResponse.json({ product })
-// }
--- app/flights/FlightCard.tsx
+++ app/flights/FlightCard.tsx
@@ -1,11 +1,14 @@
 'use client';
 import { FlightData } from '@/types/types';
+import { useRouter } from 'next/navigation';
 
 type Props = {
   flight: FlightData;
 };
 
 const FlightCard = ({ flight }: Props) => {
+  const router = useRouter();
+
   return (
     <div className="flex flex-col align-center mt-10 shadow-lg w-[300px] max-w-full bg-white p-5">
       <p className="font-bold mb-3">Flight number: {flight.flight_number}</p>
@@ -15,10 +18,23 @@ const FlightCard = ({ flight }: Props) => {
       <p className="mt-3">Longitude: {flight.lng}</p>
       <p className="mt-3">Speed: {flight.speed} kmph</p>
       <p className="mt-3">Elevation: {flight.alt} meters</p>
+      <p className="mt-3">Departure Airport IATA code: {flight.dep_iata}</p>
+      <button
+        className="px-5 py-2.5 font-medium bg-blue-50 hover:bg-blue-100 hover:text-blue-600 text-blue-500 rounded-lg text-sm"
+        onClick={() => router.push(`/airports/${flight.dep_iata}`)}
+      >
+        View Departure aiport details
+      </button>
+      <p className="mt-3">Arrival Airport IATA code: {flight.arr_iata}</p>
+      <button
+        className="px-5 py-2.5 font-medium bg-blue-50 hover:bg-blue-100 hover:text-blue-600 text-blue-500 rounded-lg text-sm"
+        onClick={() => router.push(`/airports/${flight.arr_iata}`)}
+      >
+        View Arrival aiport details
+      </button>
       <p className="mt-3">Departure Airport ICAO code: {flight.dep_icao}</p>
       <p className="mt-3">Arrival Airport ICAO code: {flight.arr_icao}</p>
     </div>
   );
 };
-
 export default FlightCard;
