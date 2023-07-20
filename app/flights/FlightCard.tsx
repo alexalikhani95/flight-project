334e2ca276e04bced7e6a3c88f9ab54353ffa9ec
Alex <alikhanialex@gmail.com>
2023-07-20 22:38:48 +0100

    Adding button in a flight card to be able to see the flights location on a map and updating the map page to use latitude and longititude from the url string for this

--- app/flights/FlightCard.tsx
+++ app/flights/FlightCard.tsx
@@ -12,13 +12,30 @@ const FlightCard = ({ flight }: Props) => {
   const { user } = useContext(UserContext) as UserContextType;
   const router = useRouter();
 
+  const handleViewOnMap = () => {
+    const latitude = flight.lat;
+    const longitude = flight.lng;
+    const mapUrl = `/map/latitude=${latitude}&longitude=${longitude}`;
+
+    // Navigate to the Map page with the latitude and longitude as query parameters
+    router.push(mapUrl);
+  };
+
   return (
     <div className="flex flex-col align-center mt-10 shadow-lg w-[300px] max-w-full bg-white p-5">
       <p className="font-bold mb-3">Flight number: {flight.flight_number}</p>
       <p className="mt-3">Status: {flight.status}</p>
       <p className="mt-3">Aircraft Registration Number: {flight.reg_number}</p>
+
       <p className="mt-3">Latitude: {flight.lat}</p>
       <p className="mt-3">Longitude: {flight.lng}</p>
+      <button
+        className="px-5 py-2.5 font-medium bg-blue-50 hover:bg-blue-100 hover:text-blue-600 text-blue-500 rounded-lg text-sm"
+        onClick={handleViewOnMap}
+      >
+        View Location on map
+      </button>
+
       <p className="mt-3">Speed: {flight.speed} kmph</p>
       <p className="mt-3">Elevation: {flight.alt} meters</p>
       <p className="mt-3">Departure Airport IATA code: {flight.dep_iata}</p>
