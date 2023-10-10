0e7efbd966e8881f0039426372758566e94dda23
Alex <alikhanialex@gmail.com>
2023-10-10 19:53:56 +0100

    Refactoring the functionality to get user data in the dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,9 +4,9 @@ import Link from 'next/link';
 import { useCallback, useContext, useEffect, useState } from 'react';
 import { UserContext, UserContextType } from '../context/UserContext';
 import {db} from '../firebase';
-import { collection, getDocs, query, where } from 'firebase/firestore';
+import { doc, getDoc } from 'firebase/firestore';
 
-type userType = {
+type User = {
   email: string;
   id?: string;
   age?: string;
@@ -16,33 +16,25 @@ type userType = {
 const Dashboard = () => {
   const { user } = useContext(UserContext) as UserContextType;
 
-  const [userInfo, setUserInfo] = useState<userType | null>(null);
+  const [userData, setUserData] = useState<User | null>(null);
 
+  const getUser = useCallback(async () => {
+    const docRef = doc(db, "users", user?.uid as string);
+    const docSnap = await getDoc(docRef);
+
+    setUserData(docSnap.data() as User);
+
+  },[user?.uid])
   
-  
-  const getUserInfo = useCallback(async () => {
-    const usersRef = collection(db, "users");
-    const q = query(usersRef, where("email", "==", user?.email));
-
-    const querySnapshot = await getDocs(q);
-
-    const usersArray: userType[] = querySnapshot.docs
-      .map(doc => ({ id: doc.id, ...doc.data() } as userType));
-      
-      setUserInfo(usersArray.length > 0 ? usersArray[0] : null);
-
-  }, [user?.email])
-  
-
   useEffect(() => {
-    getUserInfo()
-  },[getUserInfo])
+    getUser()
+  },[getUser])
   
   return (
     <div className="flex items-center flex-col">
       <h2 className='text-3xl font-bold"'>Welcome {user?.displayName}</h2>
-      {userInfo && userInfo.age && <h3>Age: {userInfo.age}</h3>}
-      {userInfo && userInfo.location && <h3>Location: {userInfo.location}</h3>}
+      {userData && userData.age && <h3>Age: {userData.age}</h3>}
+      {userData && userData.location && <h3>Location: {userData.location}</h3>}
       <Link href="/airports">
         <button className="mt-10 bg-white p-5 border-2 border-blue-950">
           Airports
